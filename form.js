7cf20c2d0e837bc6f5da562554383ee2f6880d45
Greeshma Reddy <63574109+Greeshma-Reddy@users.noreply.github.com>
2021-01-21 21:37:30 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,6 +1,4 @@
-
-
-  // Your web app's Firebase configuration
+// Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyBDX3FAYWLhPBufLambeirdxgmiISyvHMo",
     authDomain: "udagra-f0fea.firebaseapp.com",
@@ -13,18 +11,20 @@
   firebase.initializeApp(firebaseConfig);
 
 	const auth = firebase.auth();
-	
-	
+  
+ 
 	function signUp(){
 		
 		var email = document.getElementById("email");
 		var password = document.getElementById("password");
 		
 		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
+    /*promise.catch(e => alert(e.message));*/
 		
 		alert("Signed Up");
-	}
+  }
+  
+ 
 
 	function signIn(){
 		
@@ -34,10 +34,8 @@
 		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
     promise.catch(e => alert(e.message));
     document.location.href = "page1.html";
-
-		
-	}
 	
+	}
 	
 	function signOut(){
 		
@@ -46,8 +44,6 @@
 		alert("Signed Out");
 		
 	}
-	
-	
 	
 	auth.onAuthStateChanged(function(user){
 		
@@ -60,9 +56,17 @@
 
 			//is signed in
 			
-		}
+		}/*else{
+			
+			alert("No Active User");
+			//no user is signed in
+		}*/
+		
+		
 		
 	});
+	
+
 	
 
   
